157b8eb5adb77b94df37760b20bfbf27e6edccc0
Felipe González <felipexgonzalez@gmail.com>
2012-04-06 21:20:21 -0500

    Agregar variable estado, simplificar covariables

--- src/11_primarias_modelo.R
+++ src/11_primarias_modelo.R
@@ -30,15 +30,18 @@ x.pc <- as.numeric(scale(primarias.s$p_VPH_PC))
 
 ## Variables centradas por grado de marginación
 primarias.s <- ddply(primarias, c('marginación'), transform,
-    pc.cent = p_VPH_INTER - mean(p_VPH_INTER),
-    internet.cent = p_VPH_INTER - mean(p_VPH_INTER),
-    lavadora.cent = p_VPH_LAVAD - mean(p_VPH_LAVAD),
-    celular.cent = p_VPH_CEL - mean(p_VPH_CEL))
+    pc.cent = p_VPH_PC - mean(p_VPH_PC, na.rm = TRUE),
+    internet.cent = p_VPH_INTER - mean(p_VPH_INTER, na.rm = TRUE),
+    lavadora.cent = p_VPH_LAVAD - mean(p_VPH_LAVAD, na.rm = TRUE),
+    celular.cent = p_VPH_CEL - mean(p_VPH_CEL, na.rm = TRUE),
+    .progress = 'text')
 
 pc.cent <- primarias.s$pc.cent
 internet.cent <- primarias.s$internet.cent
 lavadora.cent <- primarias.s$lavadora.cent
 celular.cent <- primarias.s$celular.cent
+estado <- primarias.s$ENTIDAD
+n.estados <- 32
 
 jags.inits <- function(){
      list('a.marg'=rep(0,n.marg), 'a.tipo'=rep(0,n.tipo), 
@@ -49,16 +52,16 @@ jags.inits <- function(){
  }
 
 jags.data <- c('no.bueno.esp', 'no.bueno.mat', 'no.eval', 'no.escuelas', 'tipo', 'marg', 'n.marg',
-    'n.tipo','pc.cent', 'internet.cent', 'lavadora.cent', 'celular.cent')
+    'n.tipo','pc.cent', 'internet.cent', 'lavadora.cent', 'celular.cent', 'estado', 'n.estados')
 
 jags.params <- c('a.marg.adj', 'a.tipo.adj', 'a.adj', 'sigma.mat', 'sigma.esp',
-    'tau.mat', 'tau.esp', 'p.mat.bound', 'p.esp.bound','a.pc','a.internet','a.lavadora','a.celular')
+    'tau.mat', 'tau.esp', 'p.mat.bound', 'p.esp.bound','a.pc','a.internet','a.estado')
 
 jags.fit <- jags(model.file = './src/modelo_logit.model', 
     data = jags.data, inits = jags.inits, 
     parameters.to.save = jags.params,
     n.chains = 2, n.burnin=1000, n.thin = 5, DIC = FALSE,
-    n.iter = 10000)
+    n.iter = 6000)
 
 medias.escuelas <- jags.fit$BUGSoutput$mean$p.bound  
 hist(medias.escuelas)
