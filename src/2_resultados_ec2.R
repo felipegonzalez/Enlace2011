45b1949b162fd36fc93a0799fcfdfe041789d109
Felipe González <felipexgonzalez@gmail.com>
2012-04-10 00:41:35 -0500

    Mejorar gráficas de exploración de ranking

--- src/2_resultados_ec2.R
+++ src/2_resultados_ec2.R
@@ -1,35 +1,48 @@
 library(ProjectTemplate)
 library(ggplot2)
 library(plyr)
-load('./out/primarias_s_Entrega_abril_7.RData')
-
-primarias.salida <- primarias.s.comp[ , c('CVE_ESCUELA', 'TURNO','ESCUELA','TIPO_ESCUELA','ENTIDAD','NOM_ENT.1',
-  'tipo','marginación','tot.primaria','no.bueno.esp','no.bueno.mate', 'score', 'p.español','p.mate')]
-
-save(primarias.salida, file='./out/primarias_salida_7abril.RData')
-
+load('./out/primarias_salida_Entrega_7abril.RData')
 
 # ========================
 # = Algunos diagnósticos =
 # ========================
-primarias.resumen <- ddply(primarias.salida, c('tipo', 'marginación','TURNO'), summarise,
+## 
+primarias.resumen <- ddply(primarias.salida, c('tipo', 'marginación'), summarise,
   num.esc = length(p.español),
   media.esp = mean(p.español),
   media.mate = mean(p.mate), 
   sd.esp=sd(p.español)/sqrt(length(p.español)),
-  sd.mate = sd(p.mate)/sqrt(length(p.mate)))
+  sd.mate = sd(p.mate)/sqrt(length(p.mate))
+  )
+
 
 primarias.resumen <- subset(primarias.resumen, num.esc > 5)
+svg(file = './graph/matematicas_tipo_marginacion.svg')
+ggplot(primarias.resumen, aes(x=marginación, y=media.mate, ymax=media.mate+3*sd.mate, ymin=media.mate-3*sd.mate, 
+  colour=tipo, group=tipo)) + geom_linerange() + 
+  geom_point() + geom_line() + ylim(c(0,0.8))
+dev.off()
 
-ggplot(primarias.resumen, aes(x=marginación, y=media.mate, ymax=media.mate+2*sd.mate, ymin=media.mate-2*sd.mate, 
-  colour=TURNO, group=TURNO)) + geom_linerange() + 
-  geom_point() + geom_line() + facet_wrap(~tipo)
+svg(file = './graph/español_tipo_marginacion.svg')
+ggplot(primarias.resumen, aes(x=marginación, y=media.esp, ymax=media.esp+3*sd.esp, ymin=media.esp-3*sd.esp, 
+  colour=tipo, group=tipo)) + geom_linerange() + 
+  geom_point() + geom_line() + ylim(c(0,0.8))
+dev.off()
 
-
-ggplot(primarias.salida, aes(x=marginación, y= p.español, colour=tipo)) +
-  geom_boxplot(outlier.colour = 'gray') +  facet_wrap(~NOM_ENT.1)
-ggplot(primarias.salida, aes(x=marginación, y= p.mate, colour=tipo)) +
-  geom_boxplot(outlier.colour = 'gray') +  facet_wrap(~tipo)
-ggplot(primarias.salida, aes(x=marginación, y= score, colour=tipo)) +
-  geom_boxplot(outlier.colour = 'gray') + facet_wrap(~tipo)
+primarias.resumen.2 <- ddply(primarias.salida, c('tipo', 'marginación','score.quintil'), summarise,
+  num.esc = length(p.español),
+  media.esp = mean(p.español),
+  media.mate = mean(p.mate), 
+  sd.esp=sd(p.español)/sqrt(length(p.español)),
+  sd.mate = sd(p.mate)/sqrt(length(p.mate))
+  )
   
+primarias.resumen.2 <- ddply(primarias.resumen.2, c('tipo','marginación'), transform,
+  prop.escuelas = num.esc/sum(num.esc))
+
+svg(file = './graph/distribución_equitativa_quintiles.svg')
+ggplot(primarias.resumen.2, aes(x=marginación, fill=score.quintil, weight=prop.escuelas)) + geom_bar() +
+  facet_wrap(~tipo)
+dev.off()
+
+
